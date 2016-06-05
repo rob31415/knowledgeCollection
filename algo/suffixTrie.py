02c1c8104db11fe731d329ee8f86109c036cc60b
rob <rob@localhost.localdomain>
2016-06-05 11:32:57 +0200

    suffixTrie

---
+++ algo/suffixTrie.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+# wip: doesn't work yet
+
+
+def isSubstring(trie, aString):
+	return 0
+
+def isSuffix(trie, aString):
+	return 0
+
+def numberOfOccurrances(trie, aString):
+	return 0
+
+# substring that appears at least twice
+def longestRepeatedSubstring(trie, aString):
+	return 0
+
+def buildTrie(aString):
+	root = {}
+	for i in range(len(aString)):
+		cur = root
+		print(cur)
+		for c in aString[i:]:
+			if c not in cur:
+				cur[c]={}
+			cur = cur[c]
+	return root
+
+buildTrie("hallo")
