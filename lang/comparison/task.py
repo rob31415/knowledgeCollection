2bac0680cd082028fec865def48449a90d81c8b6
Robert Gebhardt <blabla@address.com>
2017-02-22 18:11:10 +0100

    correction

--- lang/comparison/task.py
+++ lang/comparison/task.py
@@ -5,8 +5,9 @@ input["C"] = [(1000.7,"l"), (9.3,"m"), (9.2999,"n")]
 
 output = set()
 for k,v in input.items():	# map iteration
-	# comprehension with conditional expression; lambda invocation; tuple creation
-	x = [(lambda element: element)((k,element[1])) for element in v if element[0]>=0.3 and element[0]>=9.3]
-	output |= set(x)	# add set to set
+	# A = comprehension with conditional expression (alt. to filtering); B = lambda invocation; C = tuple creation
+	#                             BC                                A                                    
+	x = [(lambda element: element)((k,element[1])) for element in v if element[0]>=0.3 and element[0]<9.3]
+	output |= set(x)	# make set from list, add that to set
 
 print(output)
--- lang/comparison/task.scala
+++ lang/comparison/task.scala
@@ -1,11 +1,7 @@
-#!/usr/bin/env scala
-
-
 object Task {
 	def main(args: Array[String]) {
-		args foreach println
 		val input = Map(
-			"A" -> List((2.3,"f"), (0.1,"g"), (0.3,"h")),
+			"A" -> List((2.3,"f"), (0.01,"g"), (0.3,"h")),
 			"B" -> List((0.3001,"i"), (0.01,"j"), (9.3,"kx")),
 			"C" -> List((1000.7,"l"), (9.3,"m"), (9.2999,"n")))
 
