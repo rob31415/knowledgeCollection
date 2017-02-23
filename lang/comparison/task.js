4294e23c54913237eb2636ec1283ebbf766ef8ff
Robert Gebhardt <blabla@address.com>
2017-02-23 14:44:51 +0100

    js task

--- lang/comparison/task.js
+++ lang/comparison/task.js
@@ -4,4 +4,10 @@ input.set("A", Array([2.3,"f"], [0.0,"g"], [0.3,"h"]))
 input.set("B", Array([0.3001,"i"], [0.01,"j"], [9.3,"kx"]))
 input.set("C", Array([1000.7,"l"], [9.3,"m"], [9.2999,"n"]))
 
-input.forEach( (v, k) => console.log(k) );
+let output = new Set()
+input.forEach( (v, k) => {
+	let x = v.filter( (el) => el[0]>=0.3 && el[0]<9.3 );
+	x.forEach( (e) => output.add({k:e[1]}) );	//TODO: how do i get k in there?
+});
+
+output.forEach( (e) => {console.log(e)} );
